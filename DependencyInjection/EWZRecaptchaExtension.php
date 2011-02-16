c3a2505fad712639ab3d08503a25e2d3e2046ae8
excelwebzone <hagaiarieli@Hagai-Arielis-MacBook-Pro.local>
2011-02-16 07:56:35 -0800

    Rename extensaion class to match the namespace structure

--- DependencyInjection/EWZRecaptchaExtension.php
+++ DependencyInjection/EWZRecaptchaExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 
-class RecaptchaExtension extends Extension
+class EWZRecaptchaExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
