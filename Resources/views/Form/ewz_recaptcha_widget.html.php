ab54d375fb06c216d401c089b99e575376c23bcd
excelwebzone <excelwebzone@gmail.com>
2011-12-11 07:47:46 -0800

    Fixed PHP widget

--- Resources/views/Form/ewz_recaptcha_widget.html.php
+++ Resources/views/Form/ewz_recaptcha_widget.html.php
@@ -1,12 +1,12 @@
-<?php if ($view->get('ewz_recaptcha_enabled')): ?>
+<?php if ($ewz_recaptcha_enabled): ?>
     <?php if (isset($attr['options'])): ?>
         <script type="text/javascript">
         var RecaptchaOptions = <?php echo json_encode($attr['options']) ?>;
         </script>
     <?php endif ?>
-    <script src="<?php echo $view->get('url_challenge') ?>" type="text/javascript"></script>
+    <script src="<?php echo $url_challenge ?>" type="text/javascript"></script>
     <noscript>
-        <iframe src="<?php echo $view->get('url_noscript') ?>" height="300" width="500" frameborder="0"></iframe><br/>
+        <iframe src="<?php echo $url_noscript ?>" height="300" width="500" frameborder="0"></iframe><br/>
         <textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
         <input type="hidden" name="recaptcha_response_field" value="manual_challenge"/>
     </noscript>
