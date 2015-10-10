7fe15449fa29ff822365a1619b0c1374824259a6
Michael H. Arieli <excelwebzone@gmail.com>
2015-10-10 13:19:58 -0700

    Found another typo

--- Form/Type/RecaptchaType.php
+++ Form/Type/RecaptchaType.php
@@ -77,7 +77,7 @@ class RecaptchaType extends AbstractType
 
         if (!$this->ajax) {
             $view->vars = array_replace($view->vars, array(
-                'url_challenge' => sprintf('%s?hl=%s', self::RECAPTCHA_API_SERVER, $options['langauge']),
+                'url_challenge' => sprintf('%s?hl=%s', self::RECAPTCHA_API_SERVER, $options['language']),
                 'public_key'    => $this->publicKey,
             ));
         } else {
