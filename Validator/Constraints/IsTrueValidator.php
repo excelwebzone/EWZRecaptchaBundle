ec7c3374c91eb1c117ac9f29741fed3c37b23ce6
Colin Burgess <colzaa@users.noreply.github.com>
2016-05-16 17:06:44 +0100

    Updated to work with PHP7

--- Validator/Constraints/IsTrueValidator.php
+++ Validator/Constraints/IsTrueValidator.php
@@ -7,7 +7,7 @@ use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\ValidatorException;
 
-class TrueValidator extends ConstraintValidator
+class IsTrueValidator extends ConstraintValidator
 {
     protected $container;
     protected $cache;
