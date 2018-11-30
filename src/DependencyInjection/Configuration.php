26ed7879c2d762376842141eb507468cf0b734f5
Michael Babker <michael.babker@gmail.com>
2018-11-30 13:16:17 -0600

    Update bundle configuration for Symfony 4.2 change

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -16,8 +16,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ewz_recaptcha');
+        $treeBuilder = new TreeBuilder('ewz_recaptcha');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('ewz_recaptcha');
+        }
 
         $rootNode
             ->children()
