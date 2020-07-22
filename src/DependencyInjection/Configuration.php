60c4675c68d3f60b9cefac0bc3163fcb4751ebd8
Raul Castellanos <raul.c@geopagos.com>
2020-07-22 00:02:01 -0300

    Change node type for better runtime symfony environment behaviour

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -29,7 +29,7 @@ class Configuration implements ConfigurationInterface
             ->children()
                 ->scalarNode('public_key')->isRequired()->end()
                 ->scalarNode('private_key')->isRequired()->end()
-                ->booleanNode('enabled')->defaultTrue()->end()
+                ->scalarNode('enabled')->defaultTrue()->end()
                 ->booleanNode('verify_host')->defaultFalse()->end()
                 ->booleanNode('ajax')->defaultFalse()->end()
                 ->scalarNode('locale_key')->defaultValue('%kernel.default_locale%')->end()
