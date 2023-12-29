feed3f2bc4a6cd4cb163253ed4c0319d0664c6c8
snipershady <snipershady@gmail.com>
2023-12-29 18:34:25 +0000

    Fix type

--- src/Validator/Constraints/IsTrueValidator.php
+++ src/Validator/Constraints/IsTrueValidator.php
@@ -41,11 +41,10 @@ class IsTrueValidator extends ConstraintValidator
     protected bool $verifyHost;
 
     /**
-     * Authorization Checker.
-     *
-     * @var AuthorizationChecker
+     * 
+     * @var AuthorizationChecker|null
      */
-    protected AuthorizationChecker $authorizationChecker;
+    protected ?AuthorizationCheckerInterface $authorizationChecker;
 
     /**
      * Trusted Roles.
